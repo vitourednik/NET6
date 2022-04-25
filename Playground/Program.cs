401d847997384dad38ee3523dcd0e4c4f62c47c3
vitourednik <vit.ourednik@samohyl.cz>
2022-04-25 11:19:37 +0200

    commit prohozeni cisel

---
+++ Playground/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int cislo = -13 + 22;
+
+int a = 10;
+
+int b = 20;
+
+int pom;
+
+double desetinne_cislo = 305.1234;
+
+string retezec = "Ahoj světe!";
+
+char pismenko = 'A';
+
+pom = a;
+a = b;
+b = pom;
+Console.WriteLine(a);
+
+
