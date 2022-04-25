d5e2b0c6c68eb4badbc6cbf6908b7a5b7767066c
vitourednik <vit.ourednik@samohyl.cz>
2022-04-25 12:05:44 +0200

    prepocet cels na far

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -14,7 +14,7 @@ string retezec = "Ahoj světe!";
 
 char pismenko = 'A';*/
 
-Console.WriteLine("Zadejte teplotu ve stupních celsia a zmáčkni enter (desetinné číslo): ");
+/*Console.WriteLine("Zadejte teplotu ve stupních celsia a zmáčkni enter (desetinné číslo): ");
 
 string vstup = Console.ReadLine();
 
@@ -25,6 +25,23 @@ double farenheit = (1.8 * celsius) + 32;
 //pom = a;
 //a = b;
 //b = pom;
-Console.WriteLine("Hodnota ferenheita je: " + farenheit);
+Console.WriteLine("Hodnota ferenheita je: " + farenheit);*/
+Console.WriteLine("zadejte prvni cele cislo a zmackni enter");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("zadejte druhe cele cislo a zmackni enter");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("zadejte treti cele cislo a zmackni enter");
+int c = int.Parse(Console.ReadLine());
 
-
+if (a > b & a > c)
+{
+    Console.WriteLine("prvni cislo je nejvetsi");
+}
+else if (b > c & b > a)
+{
+    Console.WriteLine("druhe cislo je nejvetsi");
+}
+else if (c > a & c > b)
+{
+    Console.WriteLine("treti cislo je nejvetsi");
+}
