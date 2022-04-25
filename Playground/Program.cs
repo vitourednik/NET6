3634b414de66bac7e7d2969d7720e9f1632ab1a8
vitourednik <vit.ourednik@samohyl.cz>
2022-04-25 13:33:39 +0200

    den v tydnu

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -1,86 +1,41 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-/*int cislo = -13 + 22;
+Console.WriteLine("Zadejte hodnotu 1-7 pro cislo dne:");
+int vstup = int.Parse(Console.ReadLine());
 
-int a = 10;
-
-int b = 20;
-
-int pom;
-
-double desetinne_cislo = 305.1234;
-
-string retezec = "Ahoj světe!";
-
-char pismenko = 'A';*/
-
-/*Console.WriteLine("Zadejte teplotu ve stupních celsia a zmáčkni enter (desetinné číslo): ");
-
-string vstup = Console.ReadLine();
-
-double celsius = double.Parse(vstup);
-
-double farenheit = (1.8 * celsius) + 32;
-
-//pom = a;
-//a = b;
-//b = pom;
-Console.WriteLine("Hodnota ferenheita je: " + farenheit);*/
-Console.WriteLine("zadejte prvni cele cislo a zmackni enter");
-int a = int.Parse(Console.ReadLine());
-Console.WriteLine("zadejte druhe cele cislo a zmackni enter");
-int b = int.Parse(Console.ReadLine());
-Console.WriteLine("zadejte treti cele cislo a zmackni enter");
-int c = int.Parse(Console.ReadLine());
-
-if (a > b)
+if (vstup == 1)
 {
-if (a > c)
-    {
-        Console.WriteLine("prvni cislo je nejvetsi");
-    }
-else
-    {
-        Console.WriteLine("treti cislo je nejvetsi");
-    }
+    Console.WriteLine("pondeli");
 }
-else if (b > c)
+else if (vstup == 2)
 {
-    Console.WriteLine("druhe cislo je nejvetsi");
+    Console.WriteLine("utery");
 }
-else
+else if (vstup == 3)
 {
-    Console.WriteLine("treti cislo je nejvetsi");
+    Console.WriteLine("streda");
+}
+else if (vstup == 4)
+{
+    Console.WriteLine("ctvrtek");
+}
+else if (vstup == 5)
+{
+    Console.WriteLine("patek");
+}
+else if (vstup == 6)
+{
+    Console.WriteLine("sobota");
+}
+else if (vstup == 7)
+{
+    Console.WriteLine("nedele");
+}
+else 
+{
+    Console.WriteLine("mimo hodnotu");
 }
 
-/*if (a > b && a > c)
-{
-    Console.WriteLine("prvni cislo je nejvetsi");
-}
-else if (b > c && b > a)
-{
-    Console.WriteLine("druhe cislo je nejvetsi");
-}
-else if (c > a && c > b)
-{
-    Console.WriteLine("treti cislo je nejvetsi");
-}
-
-else if (a == b && c < a)
-{
-    Console.WriteLine("prvni a druhe cislo jsou nejvetsi");
-}
-else if (a == c && b < a)
-{
-    Console.WriteLine("prvni a treti cisla jsou nejvetsi");
-}
-else if (b == c && a < b)
-{
-    Console.WriteLine("druhe a treti cisla jsou nejvetsi");
-}
-else if (a == b && b == c)
-{
-    Console.WriteLine("vsechna 3 cisla jsou stejne velka");
-}*/
 
 
+Console.WriteLine(vstup);
