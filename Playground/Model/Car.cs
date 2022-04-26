fbd34e5d27b4632ea21f497b02a229eebb3bb0d6
vitourednik <vit.ourednik@samohyl.cz>
2022-04-26 14:03:51 +0200

    trida car

---
+++ Playground/Model/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Playground.Model
+{
+    internal class Car
+    {
+        public Car()
+        {
+
+        }
+
+        public string SPZ { get; set; }
+
+        public string Barva { get; set; }
+
+        public double NajeteKm { get; set; }
+
+        public string Znacka { get; set; }
+
+    }
+}
