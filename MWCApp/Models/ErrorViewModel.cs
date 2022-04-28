2f04c9ed540c49d3eb7538e6ef9e20934496e5af
vitourednik <vit.ourednik@samohyl.cz>
2022-04-28 13:45:42 +0200

    mwc projekt

---
+++ MWCApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MWCApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
